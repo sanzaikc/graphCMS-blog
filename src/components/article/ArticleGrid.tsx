64d0c19dfa12586500a9b62b3e3659128e24a800
Sanjay <sanzaikc@gmail.com>
2022-06-15 09:49:58 +0545

    article grid ts

--- src/components/article/ArticleGrid.tsx
+++ src/components/article/ArticleGrid.tsx
@@ -1,5 +1,26 @@
 import React from "react";
 
-export const ArticleGrid = ({ children }: React.PropsWithChildren) => {
-  return <div className="grid grid-cols-3 gap-4">{children}</div>;
+import { Article } from "../../interfaces/article.interface";
+
+interface ArticleGridProps {
+  articles: Article[];
+}
+
+export const ArticleGrid: React.FC<ArticleGridProps> = ({
+  articles,
+}: ArticleGridProps) => {
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      {articles.map((article: any) => (
+        <div key={article.id} className="bg-white">
+          <p>{article.title}</p>
+          <img
+            src={article.coverImage.url}
+            alt="Cover"
+            className="h-2/3 w-1/4"
+          />
+        </div>
+      ))}
+    </div>
+  );
 };
--- src/pages/Articles.tsx
+++ src/pages/Articles.tsx
@@ -7,20 +7,7 @@ import { useGetArticles } from "../hooks/article/useGetArticles";
 const Articles: React.FC = () => {
   const articles = useGetArticles() || [];
 
-  return (
-    <ArticleGrid>
-      {articles.map((article: any) => (
-        <div key={article.id} className="bg-white">
-          <p>{article.title}</p>
-          <img
-            src={article.coverImage.url}
-            alt="Cover"
-            className="h-2/3 w-1/4"
-          />
-        </div>
-      ))}
-    </ArticleGrid>
-  );
+  return <ArticleGrid articles={articles}></ArticleGrid>;
 };
 
 export default Articles;
