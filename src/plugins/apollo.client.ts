715d5fd15be3fcd5700ccf4e051cd957936ed5cd
Sanjay <sanzaikc@gmail.com>
2022-06-17 12:06:04 +0545

    mend

--- src/plugins/apollo.client.ts
+++ src/plugins/apollo.client.ts
@@ -1,9 +1,18 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { persistCache, LocalStorageWrapper } from "apollo3-cache-persist";
 
 const GRAPHQL_URI =
   "https://api-ap-south-1.graphcms.com/v2/cl4c541jr3lb701z66q4b6y6k/master";
 
+const cache = new InMemoryCache();
+
+// persisting cache for offline use
+persistCache({
+  cache,
+  storage: new LocalStorageWrapper(window.localStorage),
+});
+
 export const client = new ApolloClient({
+  cache,
   uri: GRAPHQL_URI,
-  cache: new InMemoryCache(),
 });
