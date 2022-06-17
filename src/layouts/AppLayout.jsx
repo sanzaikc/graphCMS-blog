0f62b1acf2624626add60c8e814a2b550ab6ce71
Sanjay <sanzaikc@gmail.com>
2022-06-17 12:28:27 +0545

    scroll to top when  page change

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,9 +1,19 @@
-import { Outlet } from "react-router";
+import { useLayoutEffect } from "react";
+import { Outlet, useLocation } from "react-router";
 
 import AppHeader from "../components/header/AppHeader";
 import NetworkStatus from "../components/NetworkStatus";
 
 export default function AppLayout() {
+  const location = useLocation();
+
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [location.pathname]);
+
   return (
     <div className="relative">
       <AppHeader />
